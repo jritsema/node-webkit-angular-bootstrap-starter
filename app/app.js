671743f801c9d3104528f457a1034c6d51a34988
John Ritsema <john_ritsema@yahoo.com>
2014-03-01 22:36:21 -0500

    added angular scaffolding with routes

---
+++ app/app.js
@@ -0,0 +1,12 @@
+﻿'use strict'
+
+var app = angular.module('MyApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/view1', {
+			templateUrl: 'app/view1.html', 
+			controller: 'view1Controller'
+		})
+		.otherwise({  redirectTo: '/view1' });
+});
---
+++ app/view1.js
@@ -0,0 +1,7 @@
+﻿'use strict'
+
+app.controller('view1Controller', function ($scope) {
+
+	$scope.title = 'View 1';
+
+});
