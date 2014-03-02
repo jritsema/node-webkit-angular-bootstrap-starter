36a8597a56df73fec57e55669fb267eb96162a24
John Ritsema <john_ritsema@yahoo.com>
2014-03-02 00:09:20 -0500

    show file system contents using node fs

--- app/main.js
+++ app/main.js
@@ -1,5 +1,15 @@
 ﻿'use strict'
 
-app.controller('mainController', function ($scope) {
+app.controller('mainController', function ($scope, $q) {
+
+	$scope.nodeVersion = process.version;
+
+	var fs = require('fs');
+	fs.readdir('.', function (err, files) {
+		if (err) throw err;
+		$scope.$apply(function() { 
+			$scope.files = files;
+		});
+	});
 
 });
