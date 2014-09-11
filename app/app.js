b0842a4e61c9380cfe2c9c25f99cf7dcd5fe4edb
John Ritsema <john_ritsema@yahoo.com>
2014-09-11 18:00:37 -0400

    converted tabs to 2 spaces

--- app/app.js
+++ app/app.js
@@ -1,19 +1,19 @@
 ﻿var app = angular.module('MyApp', ['ngRoute']);
 
 (function () {
-	'use strict';	
+  'use strict'; 
 
-	app.config(function ($routeProvider) {
-		$routeProvider
-			.when('/home', {
-				templateUrl: 'home/home.html', 
-				controller: 'homeController'
-			})
-			.when('/about', {
-				templateUrl: 'about/about.html', 
-				controller: 'aboutController'
-			})		
-			.otherwise({  redirectTo: '/home' });
-	});	
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/home', {
+        templateUrl: 'home/home.html', 
+        controller: 'homeController'
+      })
+      .when('/about', {
+        templateUrl: 'about/about.html', 
+        controller: 'aboutController'
+      })    
+      .otherwise({  redirectTo: '/home' });
+  }); 
 
 })();
--- app/home/home.js
+++ app/home/home.js
@@ -1,18 +1,18 @@
 ﻿(function () {
-	'use strict';
+  'use strict';
 
-	app.controller('homeController', function ($scope) {
+  app.controller('homeController', function ($scope) {
 
-		var gui = require('nw.gui');
-		var os = require('os');	
+    var gui = require('nw.gui');
+    var os = require('os'); 
 
-		$scope.description = gui.App.manifest.description;
-		$scope.settings = [];
+    $scope.description = gui.App.manifest.description;
+    $scope.settings = [];
 
-		Object.getOwnPropertyNames(os).forEach(function(val, idx, array) { 
-			if (typeof os[val] === 'function')
-				$scope.settings.push({ name: val, value: os[val]() });	
-		});
-	});
+    Object.getOwnPropertyNames(os).forEach(function(val, idx, array) { 
+      if (typeof os[val] === 'function')
+        $scope.settings.push({ name: val, value: os[val]() });  
+    });
+  });
 
 })();
