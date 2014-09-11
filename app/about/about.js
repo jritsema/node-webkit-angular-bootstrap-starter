a542a22b82466e1f6f310850671af11cede17b57
John Ritsema <john_ritsema@yahoo.com>
2014-09-11 17:56:18 -0400

    added 'use strict'

--- app/about/about.js
+++ app/about/about.js
@@ -1,13 +1,15 @@
-﻿'use strict'
+﻿(function () {
+	'use strict';
 
-app.controller('aboutController', function ($scope) {
+	app.controller('aboutController', function ($scope) {
 
-	var gui = require('nw.gui');
-	var manifest = gui.App.manifest;
-	$scope.name = manifest.name;
-	$scope.description = manifest.description;
-	$scope.appVersion = manifest.version;
-	$scope.nodeVersion = process.version;
-	$scope.nodeWebkitVersion = process.versions['node-webkit'];
+		var gui = require('nw.gui');
+		var manifest = gui.App.manifest;
+		$scope.name = manifest.name;
+		$scope.description = manifest.description;
+		$scope.appVersion = manifest.version;
+		$scope.nodeVersion = process.version;
+		$scope.nodeWebkitVersion = process.versions['node-webkit'];
+	});
 
-});
+})();
--- app/app.js
+++ app/app.js
@@ -1,16 +1,19 @@
-﻿'use strict'
+﻿var app = angular.module('MyApp', ['ngRoute']);
 
-var app = angular.module('MyApp', ['ngRoute']);
+(function () {
+	'use strict';	
 
-app.config(function ($routeProvider) {
-	$routeProvider
-		.when('/home', {
-			templateUrl: 'home/home.html', 
-			controller: 'homeController'
-		})
-		.when('/about', {
-			templateUrl: 'about/about.html', 
-			controller: 'aboutController'
-		})		
-		.otherwise({  redirectTo: '/home' });
-});
+	app.config(function ($routeProvider) {
+		$routeProvider
+			.when('/home', {
+				templateUrl: 'home/home.html', 
+				controller: 'homeController'
+			})
+			.when('/about', {
+				templateUrl: 'about/about.html', 
+				controller: 'aboutController'
+			})		
+			.otherwise({  redirectTo: '/home' });
+	});	
+
+})();
